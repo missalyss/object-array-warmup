1212458fb7d7ab3565e04bab4284a426b06f2260
DerekWW <derek.wierson@gmail.com>
2017-02-21 08:10:06 -0800

    add object problem

--- main.js
+++ main.js
@@ -3,4 +3,25 @@
   keeps track of how many times a character is used across all indexes of the array.
   Console log the object and run this file with node in your command line by running 'node main.js'.
 */
+
+//Use this array as your input. 🐢
 let stringArr = ['this','is', 'my favorite', 'way', 'to start', 'my day at', 'galvanize'];
+
+
+
+
+
+
+
+/*
+  Write a function that takes a object and flips the key value pairs. Log out the object once you are done. Use the
+  object below and run the file with 'node main.js' 
+*/
+let funObj = {
+  name: 'Donald',
+  species: 'Duck',
+  age: 51,
+  cartoon: true,
+  employer: 'Disney',
+  occupation: 'Sailor'
+};
