bfa0f7aafd7be209ab0855cd0b2ff4e557fb8715
DerekWW <derek.wierson@gmail.com>
2017-02-21 07:59:23 -0800

    add warmup

---
+++ main.js
@@ -0,0 +1,6 @@
+/*
+  Write a function that takes an array of strings. Create a object that
+  keeps track of how many times a character is used across all indexes of the array.
+  Console log the object and run this file with node in your command line by running 'node main.js'.
+*/
+let stringArr = ['this','is', 'my favorite', 'way', 'to start', 'my day at', 'galvanize'];
